7b84632ed7ffc236054726af3affa7b1ccf935cf
kikiexe <putraajril2@gmail.com>
2026-03-24 22:32:19 +0700

    Fix horizontal scroll overflow globally

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased overflow-x-hidden w-full max-w-[100vw]">
         @inertia
     </body>
 </html>
